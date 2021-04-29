076f7ff48e854f446ee6d6336822039bfd3d1504
clairepeng <zdimple0808@gmail.com>
2021-04-29 16:50:51 +0800

    test test

--- src/firebase/firebaseUtils.js
+++ src/firebase/firebaseUtils.js
@@ -31,7 +31,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       console.log('Error creating user', error.message);
     }
   }
-  return userRef;
+  return firestore;
 };
 
 const provider = new firebase.auth.GoogleAuthProvider();
